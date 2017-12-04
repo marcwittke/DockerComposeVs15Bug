9a4f6c5096356d9f41ef779e1cba4b7b72b7f089
Marc Wittke <marc@wittke-web.de>
2017-12-04 08:52:59 -0300

    Reproduces the NRE on build with docker-compose project

---
+++ src/WebApplication5.Domain/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WebApplication5.Domain
+{
+    using System.Collections.Generic;
+    using System.Data;
+
+    public class Class1
+    {
+        public string[] GetDatabases(string masterConnectionString)
+        {
+            List<string> databases = new List<string>();
+            
+            using (SqlConnection connection = new SqlConnection(masterConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Name FROM sys.Databases";
+                    var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    while (reader.Read())
+                    {
+                        databases.Add(reader.GetString(0));
+                    }
+                }
+            }
+            return databases.ToArray();
+        } 
+    }
+}
---
+++ src/WebApplication5/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication5
+{
+    using Domain;
+    using Microsoft.Extensions.Configuration;
+
+    public class Startup
+    {
+        private IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                    .AddEnvironmentVariables();
+
+            builder.AddApplicationInsightsSettings(developerMode: env.IsDevelopment());
+
+            configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) => {
+                        var databases = new Class1().GetDatabases(configuration.GetConnectionString("MasterDatabase"));
+                        string databasesString = string.Join(Environment.NewLine, databases);
+                        await context.Response.WriteAsync(databasesString);
+                    });
+        }
+    }
+}
